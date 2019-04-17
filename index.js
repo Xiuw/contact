ee7876db2b45084c693b8c77b1fb8219b945801b
xiuw <sylvi.xw@gmail.com>
2019-04-17 01:19:26 -0400

    contact form

---
+++ index.js
@@ -0,0 +1,58 @@
+const nodemailer = require('nodemailer');
+const express = require('express');
+const bodyParser=require('body-parser');
+const cors = require('cors');
+
+const app =express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+	res.send('It worked');
+})
+
+const transport = {
+	service:"Gmail",
+	auth:{
+		user:process.env.myEmail,
+		pass:process.env.myPassword
+	}
+}
+
+const transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+app.post("/contact", (req, res)=>{
+  console.log("I got here");
+  const {name, email, message} = req.body;
+  console.log(name);
+  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+  let mail = {
+    from: name,
+    to: process.env.myEmail,  //Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        msg: 'fail'
+      })
+    } else {
+      res.json({
+        msg: 'success'
+      })
+    }
+  })
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+	console.log(`Running on Port ${process.env.Port}`);
+})
